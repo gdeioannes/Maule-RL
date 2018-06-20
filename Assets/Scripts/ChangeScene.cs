15d0d357b9108ec75939c51a50b9dd6b52ed8008
Gabriel de Ioannes Becker <gdeioannes@gmail.com>
2018-06-20 00:35:30 -0300

    Scene connection and minimum particles

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void goToScene(string name){
+		SceneManager.LoadScene(name);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
